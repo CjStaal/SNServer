fb2c2e054ccbd6eb0db46f45c00deb55077ea572
Charles Staal <Charles.Staal@gmail.com>
2015-03-18 14:28:09 -0400

    Encapsulating, refactoring, and designing..

--- src/com/staalcomputingsolutions/snserver/replier/DefaultReplier.java
+++ src/com/staalcomputingsolutions/snserver/replier/DefaultReplier.java
@@ -3,10 +3,32 @@
  */
 package com.staalcomputingsolutions.snserver.replier;
 
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Charles Joseph Staal
  */
-public class DefaultReplier implements Replier{
-    
+public class DefaultReplier implements Replier {
+
+    private final OutputStream outputStream;
+    private final DataOutputStream dataOutputStream;
+
+    public DefaultReplier(OutputStream outputStream) {
+        this.outputStream = outputStream;
+        this.dataOutputStream = new DataOutputStream(outputStream);
+    }
+
+    @Override
+    public void reply(String message) {
+        try {
+            this.dataOutputStream.writeUTF(message);
+        } catch (IOException ex) {
+            Logger.getLogger(DefaultReplier.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
--- src/com/staalcomputingsolutions/snserver/replier/Replier.java
+++ src/com/staalcomputingsolutions/snserver/replier/Replier.java
@@ -9,4 +9,5 @@ package com.staalcomputingsolutions.snserver.replier;
  */
 public interface Replier {
     
+    public void reply(String message);
 }
